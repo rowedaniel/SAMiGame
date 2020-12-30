42a27d337507aa1307cc06d4dd8dac4165e8c34a
Daniel Rowe <rowedanielp@gmail.com>
2020-12-30 11:50:18 -0800

    preliminary steps for levels

---
+++ SAMiGame/Menus/LevelMenu.cpp
@@ -0,0 +1,9 @@
+#include "LevelMenu.h"
+
+LevelMenu::LevelMenu(float x, float y)
+{
+}
+
+LevelMenu::~LevelMenu()
+{
+}
--- SAMiGame/Menus/LevelMenu.h
+++ SAMiGame/Menus/LevelMenu.h
@@ -1,1 +1,10 @@
 #pragma once
+#include "../stdafx.h"
+#include "Menu.h"
+
+class LevelMenu : public Menu
+{
+public:
+	LevelMenu(float x, float y);
+	~LevelMenu();
+};
